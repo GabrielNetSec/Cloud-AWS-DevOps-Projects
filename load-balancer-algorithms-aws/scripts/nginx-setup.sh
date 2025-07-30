9ce1a704385795a8615103a79a6e4b23c134f3ad
Gabriel N <52141146+GabrielNetSec@users.noreply.github.com>
2025-07-29 23:13:34 -0500

    Update nginx-setup.sh

--- load-balancer-algorithms-aws/scripts/nginx-setup.sh
+++ load-balancer-algorithms-aws/scripts/nginx-setup.sh
@@ -1,1 +1,8 @@
+apt install -y nginx → Installs NGINX instead of Apache.
+systemctl start nginx → Starts the NGINX web server.
+systemctl enable nginx → Ensures NGINX starts on boot.
 
+echo "<h1>Welcome to my NGINX server at $(hostname -f)</h1>" > /var/www/html/index.html
+
+nano /var/www/html/index.html
+systemctl restart nginx
